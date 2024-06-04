ae3fba4d3fc202081333df4d37e12d65e3d6f881
RaquelMelow <raquelmelo949@gmail.com>
2024-06-04 18:17:47 +0100

    first commit

---
+++ firstapp/first-exercise/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import TwitterFollowCard  from './TwitterFollowCard.jsx'
+
+
+function App () {
+  
+     return (
+        <section className='App'>
+        <TwitterFollowCard isFollowing userName="midudev" name="Miguel Ángel" />
+        </section>
+     )
+}
+
+export default App;
---
+++ firstapp/first-exercise/src/TwitterFollowCard.jsx
@@ -0,0 +1,36 @@
+
+function TwitterFollowCard({ userName, name, initialIsFollowing }) {
+    const [isFollowing, setIsFollowing] = useState(initialIsFollowing)
+
+    const text = isFollowing ? 'Siguiendo' : 'Seguir';
+    const buttonClassName = isFollowing 
+    ? 'tw-followCard-button is-following'
+    : 'tw-followCard-button'
+
+    const handleClick = () => {
+        setIsFollowing(!isFollowing)
+      }
+      
+  return (
+    <article className='tw-followCard'>
+            <header className='tw-followCard-header'>
+            <img 
+            className='tw-followCard-avatar'
+            src={`https://unavatar.io/${userName}`}
+            alt='Foto de midudev' 
+            />
+            <div className='tw-followCard-info'>
+                <strong>{name}</strong>
+                <span className="tw-followCard-infoUserName">@{userName}</span>
+            </div>
+            </header>
+            <aside>
+                <button className={buttonClassName} onClick={handleClick}>
+                    {text}
+                </button>
+            </aside>
+        </article>
+  )
+}
+
+export default TwitterFollowCard
